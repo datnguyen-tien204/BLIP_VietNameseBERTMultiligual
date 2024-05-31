d1842355c6124de27fc41a3eb951686eb1bca1ed
Dat Nguyen-Tien <118755598+datnguyen-tien204@users.noreply.github.com>
2024-05-31 17:27:43 +0700

    Update blip.py

--- models/blip.py
+++ models/blip.py
@@ -184,7 +184,7 @@ def blip_feature_extractor(pretrained='',**kwargs):
 
 def init_tokenizer():
     # tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-    tokenizer = AutoTokenizer.from_pretrained("google-bert/bert-base-multilingual-cased")
+    tokenizer = AutoTokenizer.from_pretrained("google-bert/bert-base-multilingual-uncased")
     tokenizer.add_special_tokens({'bos_token':'[DEC]'})
     tokenizer.add_special_tokens({'additional_special_tokens':['[ENC]']})       
     tokenizer.enc_token_id = tokenizer.additional_special_tokens_ids[0]  
